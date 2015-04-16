5d83a733cacbb30a85a5b92c88a784b51adcb7be
Ghali <ghaliano2005@gmail.com>
2015-04-16 11:17:51 +0100

    Update demo.php

--- Demo/demo.php
+++ Demo/demo.php
@@ -5,9 +5,6 @@ require 'config_dev.php';
 use MediaGateway\Provider\ProviderChain;
 use MediaGateway\ProviderClientFactory;
 use MediaGateway\Provider\YoutubeProvider;
-use MediaGateway\Provider\VimeoProvider;
-use MediaGateway\Provider\DailymotionProvider;
-use MediaGateway\Provider\SoundcloudProvider;
 
 $youtubeProvider = new YoutubeProvider(
     ProviderClientFactory::create('youtube', $youtubeConfig)
